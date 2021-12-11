654ba9c4ac41c65777cf66df54f729c9d108fae0
jwgit2 <johannwerklegit@gmail.com>
2021-12-11 12:10:09 +0100

    NFC ajout du login

--- app/src/main/java/com/example/Labo03/NFCActivity.kt
+++ app/src/main/java/com/example/Labo03/NFCActivity.kt
@@ -3,22 +3,37 @@ package com.example.Labo3
 import android.app.PendingIntent
 import android.content.Intent
 import android.content.IntentFilter
+import android.nfc.NdefMessage
 import android.nfc.NfcAdapter
 import android.nfc.NfcAdapter.ACTION_TECH_DISCOVERED
 import android.nfc.Tag
 import android.nfc.tech.IsoDep
 import android.os.Bundle
+import android.util.Log
 import com.example.Labo03.R
 import android.widget.Button
 import android.widget.EditText
-import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class NFCActivity : AppCompatActivity() {
-    private lateinit var mNfcAdapter : NfcAdapter
+
+
+    private lateinit var mNfcAdapter: NfcAdapter
+    private lateinit var loginEdit: EditText
+    private lateinit var passwordEdit: EditText
+    private lateinit var cancelButton: Button
+    private lateinit var validateButton: Button
+    private var authenticationLevel: Int = 0
+
+    //TODO : GERER LA DECREMENTATION DU NIVEAU D'AUTHENTIFICATION
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_nfc)
         mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        setButtonsBehaviours()
 
     }
 
@@ -56,11 +71,57 @@ class NFCActivity : AppCompatActivity() {
         super.onNewIntent(intent)
 
         if (NfcAdapter.ACTION_TECH_DISCOVERED == intent.action) {
-            val tag = intent.getParcelableExtra<Tag>(NfcAdapter.EXTRA_TAG)
-            IsoDep.get(tag)?.let { isoDepTag ->
-                // Handle the tag here
-            }
+                //TODO : GERER LE CONTENU DU TAG
+                    val rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES)
+            var ndefMsg = rawMessages?.get(0) as NdefMessage
+            var ndefRecord = ndefMsg.records[0]
+            Log.d(TAG, "La balise NFC contient : " + ndefRecord.toMimeType())
+            authenticationLevel = 10;
         }
     }
 
+    private fun setButtonsBehaviours() {
+        loginEdit = findViewById(R.id.login)
+        passwordEdit = findViewById(R.id.password)
+        cancelButton = findViewById(R.id.cancel_button)
+        validateButton = findViewById(R.id.validate_button)
+
+        //mise en place des événements
+        cancelButton.setOnClickListener {
+            //on va vider les champs de la page de login lors du clique sur le bouton Cancel
+            loginEdit.text?.clear()
+            passwordEdit.text?.clear()
+            // on annule les éventuels messages d'erreur présents sur les champs de saisie
+            loginEdit.error = null
+            passwordEdit.error = null
+            return@setOnClickListener
+        }
+
+        validateButton.setOnClickListener {
+            validateButtonAction()
+            return@setOnClickListener
+        }
+    }
+    fun validateButtonAction() {
+        //on réinitialise les messages d'erreur
+        loginEdit.error = null
+        passwordEdit.error = null
+
+        //on récupère le contenu de deux champs dans des variables de type String
+       if( loginEdit.text.toString().equals(LOGIN) and passwordEdit.text.toString().equals(PASSWORD)){
+           Toast.makeText(applicationContext, "u done it gg", Toast.LENGTH_SHORT).show()
+           //TODO : CHANGER LA VUE ET ALLER SUR LE CHOIX DE SECURITE
+       }
+    }
+
+    companion object {
+        private const val AUTHENTICATE_MAX = 10
+        private const val AUTHENTICATE_MEDIUM = 6
+        private const val AUTHENTICATE_LOW = 2
+        private const val TAG: String = "NFCActivity"
+        private const val LOGIN: String = "test"
+        private const val PASSWORD: String = "test"
+    }
+
 }
+
