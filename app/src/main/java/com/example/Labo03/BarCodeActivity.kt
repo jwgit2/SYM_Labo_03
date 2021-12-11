ffb5e979b2008ac893c960233fd8df0f01acf7a6
jwgit2 <johannwerklegit@gmail.com>
2021-12-11 10:55:10 +0100

    Recréer la classe BarCode pour éviter les erreurs au lancement

---
+++ app/src/main/java/com/example/Labo03/BarCodeActivity.kt
@@ -0,0 +1,12 @@
+package com.example.Labo3
+
+import android.content.Intent
+import android.os.Bundle
+import com.example.Labo03.R
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class BarCodeActivity : AppCompatActivity() {
+}
