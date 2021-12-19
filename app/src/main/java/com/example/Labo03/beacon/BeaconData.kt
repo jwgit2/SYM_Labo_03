bd29aae8cc7e401f5befcfa8415fe3db7882c176
laurer6 <58049740+laurer6@users.noreply.github.com>
2021-12-19 18:49:19 +0100

    Update BeaconData.kt

--- app/src/main/java/com/example/Labo03/beacon/BeaconData.kt
+++ app/src/main/java/com/example/Labo03/beacon/BeaconData.kt
@@ -2,10 +2,13 @@ package com.example.Labo03.beacon
 
 import org.altbeacon.beacon.Identifier
 
-
 /**
- * data model des beacons captés
+ * Project      : SYM_Labo3
+ * Author       : Tailhades laurent, Werkle Johann, Zeller Corentin
+ * Date         : 19.12.21
+ * Description  : data model des beacons captés
  */
+
 data class BeaconData(
     val uuid: Identifier,
     val major: Identifier,
